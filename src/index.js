75e3d7ab1e08f2d04b53f1cd095cf6e8f6d1b3dd
Andrey Brill <andrey@a-brill.com>
2020-03-30 01:13:35 +0200

    Rename library

--- src/index.js
+++ src/index.js
@@ -5,7 +5,7 @@ export * from "./containers/Container.js";
 export * from "./containers/WindowContainer.js";
 export * from "./helpers/CommonProperties.js";
 export * from "./helpers/DimensionElement.js";
-export * from "./helpers/Resizers.js";
+export * from "./helpers/RcResize.js";
 export * from "./helpers/WindowResizer.js";
 export * from "./settings/FontMultipliers.js";
 export * from "./values/Rv.js";
